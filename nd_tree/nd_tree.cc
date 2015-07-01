30b1c4698a02d96886cb6d53ac3fca04dafe253b
Sangkeun Kim <sisobus1@gmail.com>
2015-07-01 17:24:51 +0900

    nd-tree add

---
+++ nd_tree/nd_tree.cc
@@ -0,0 +1,5 @@
+#include "nd_tree.h"
+int main() {
+
+    return 0;
+}
---
+++ nd_tree/nd_tree.h
@@ -0,0 +1,32 @@
+#include <cstdio>
+#include <cstring>
+#include <cstdlib>
+#include <ctime>
+#include <cmath>
+#include <cctype>
+#include <bitset>
+#include <deque>
+#include <list>
+#include <map>
+#include <queue>
+#include <set>
+#include <stack>
+#include <vector>
+#include <unordered_map>
+#include <unordered_set>
+#include <algorithm>
+#include <cassert>
+#include <functional>
+#include <string>
+#include <fstream>
+#include <sstream>
+#include <climits>
+using namespace std;
+
+#define MP make_pair
+#define PB push_back
+#define ST first
+#define ND second
+
+typedef pair<int,int> ii;
+typedef long long ll;
